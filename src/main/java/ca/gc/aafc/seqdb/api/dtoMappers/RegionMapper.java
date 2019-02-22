3297ac1e61bea77bfd05049e1b9585bc00b3a1ab
sam@sam-VirtualBox <sxuzhou@outlook.com>
2019-02-22 14:32:48 -0500

    Support #15086 Expose ThermocyclerProfile resource
    https://redmine.biodiversity.agr.gc.ca/issues/15086
    
    * Introduced the factorypath property that will help Mapstructs and
    lombok annotations generate properly.
    
    * Introduced the annotationProcessorPaths tag under the
    maven-compiler-plugin. This allows us to use both lombok and Mapstruct
    annotations.
    
    * Created a simple unit test to test the auto mapping of Mapstruct and
    the configuration of the annotations.
    
    * Created a Mapper for region using mapstructs, only implicit mappings
    currently

--- src/main/java/ca/gc/aafc/seqdb/api/dtoMappers/RegionMapper.java
+++ src/main/java/ca/gc/aafc/seqdb/api/dtoMappers/RegionMapper.java
@@ -1,17 +1,22 @@
 package ca.gc.aafc.seqdb.api.dtoMappers;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import ca.gc.aafc.seqdb.api.dto.RegionDto;
 import ca.gc.aafc.seqdb.entities.Region;
 
-@Mapper
+@Mapper(componentModel="spring")
 public interface RegionMapper {
   
   RegionMapper INSTANCE = Mappers.getMapper( RegionMapper.class);
   
+  
   RegionDto regionToRegionDto(Region region);
+  
+  @InheritInverseConfiguration
+  Region fromRegionDto(RegionDto regionDto);
   
 
 }
--- src/test/java/ca/gc/aafc/seqdb/api/mapping/Dto/MapStructTest.java
+++ src/test/java/ca/gc/aafc/seqdb/api/mapping/Dto/MapStructTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 import org.junit.Test;
 
 import ca.gc.aafc.seqdb.api.dto.RegionDto;
@@ -13,24 +18,84 @@ import ca.gc.aafc.seqdb.factories.RegionFactory;
 
 public class MapStructTest {
   
-  
-  @Test
-  public void mapstructMapRegionToDto() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-    Method meth;
-    Region region = RegionFactory.newRegion().description("About").build();
+  /**
+   * Uses reflection to compare equivalent fields using getter methods.
+   * The method with less methods should be the base argument as it will improve efficiency. 
+   * @param base
+   * @param comparison
+   * @return A String HastSet containing the methods asserted(matched) and the ones missed.
+   * @throws IllegalAccessException
+   * @throws IllegalArgumentException
+   * @throws InvocationTargetException
+   */
+  public static HashSet<String> verifyObjectsAreEqual(Object base, Object comparison)
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
     
-    RegionDto regionDto = RegionMapper.INSTANCE.regionToRegionDto(region);
-    
-    for(Method getter: RegionDto.class.getMethods()) {
-      System.out.println("DTO METHODS IS " + getter.getName());
-      if(getter.getName().contains("get"))
-      {
-        meth = region.getClass().getMethod(getter.getName());
-        System.out.println("ENTITY METHOD IS " + meth.getName());
-        assertEquals(getter.invoke(regionDto), meth.invoke(region));
+    HashSet<String> methodInformation = new HashSet<String>();
+
+    Method comparisonMethod;
+    String baseMethodName;
+
+    for (Method getterMethod : base.getClass().getMethods()) {
+
+      baseMethodName = getterMethod.getName();
+
+
+      if (baseMethodName.contains("get")) {
+        try {
+          if (comparison.getClass().getMethod(baseMethodName) != null) {
+
+            comparisonMethod = comparison.getClass().getMethod(baseMethodName);
+            if (comparisonMethod.getName() != "getClass") {
+              methodInformation.add("Asserted Method : " + comparisonMethod.getName());
+              assertEquals(getterMethod.invoke(base), comparisonMethod.invoke(comparison));
+            }
+          }
+        } catch (NoSuchMethodException e) {
+          methodInformation.add("Missed Method : " + baseMethodName);
+        }
+
       }
-      
     }
+    return methodInformation;
+  }
+
+  public static void verifyMatchingMethod(Method baseMethod, Object base, Object comparison)
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException,
+      NoSuchMethodException, SecurityException {
+    Method comparisonMethod;
+
+    if (comparison.getClass().getMethod(baseMethod.getName()) != null) {
+
+      comparisonMethod = comparison.getClass().getMethod(baseMethod.getName());
+      System.out.println("Found Matching Method : " + comparisonMethod.getName());
+      if (comparisonMethod.getName() != "getClass") {
+
+        assertEquals(baseMethod.invoke(base), comparisonMethod.invoke(comparison));
+      }
+    } else {
+      throw new IllegalArgumentException("no matching method found");
+    }
+
+  }
+
+  @Test
+  public void mapstructMapRegionToDto() throws NoSuchMethodException, SecurityException,
+      IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    Region region = RegionFactory.newRegion().description("About").build();
+
+    RegionDto regionDto = RegionMapper.INSTANCE.regionToRegionDto(region);
+    System.out.println("MADE A MAPPER");
     
+    assertEquals(regionDto.getDescription(), region.getDescription());
+    assertEquals(regionDto.getSymbol(), region.getSymbol());
+    assertEquals(regionDto.getName(), region.getName());
+    assertEquals(regionDto.getTagId(), region.getTagId());
+    
+    verifyMatchingMethod(RegionDto.class.getMethod("getSymbol"), regionDto, region);
+    System.out.println(verifyObjectsAreEqual(region,regionDto));
+
+    
+
   }
 }
